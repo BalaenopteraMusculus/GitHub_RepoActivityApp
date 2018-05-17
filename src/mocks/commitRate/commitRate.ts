43ecd44abe9080d1f4757fd3da9930a4a1310832
RK <r_kieu@outlook.com>
2018-05-16 20:37:58 -0700

    Maintenance/Added mock for commit rate

---
+++ src/mocks/commitRate/commitRate.ts
@@ -0,0 +1,15 @@
+import { CommitRate } from '../../models/commitRate/commitRate';
+
+const commitRateList: CommitRate[] = [
+    {
+        commitRate: '-'
+    },
+    {
+        commitRate: '0'
+    },
+    {
+        commitRate: '1321321'
+    }
+];
+
+export const COMMIT_RATE_LIST = commitRateList;
