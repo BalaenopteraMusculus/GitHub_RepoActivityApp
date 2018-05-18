3d9942ba387d5d192e62f89c7a3971ee91eaad6e
RK <r_kieu@outlook.com>
2018-05-17 21:19:29 -0700

    Feature/Combined commit rate with repository model

--- src/models/searchResult/searchResult.ts
+++ src/models/searchResult/searchResult.ts
@@ -1,7 +1,5 @@
 import { Repository } from "../repository/repository";
-import { CommitRate } from "../commitRate/commitRate";
 
 export interface SearchResult {
     repository: Repository;
-    commitRate: CommitRate;
 }
