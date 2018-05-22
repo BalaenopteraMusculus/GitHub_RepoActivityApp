5ef19e604e5d62437abcf249c28ebb79527d50c4
RK <r_kieu@outlook.com>
2018-05-22 16:49:56 -0700

    Maintenance/Separated login and register components into own directory for modularity

---
+++ src/components/register-form/register-form.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: 'register-form.component.html'
+})
+export class RegisterFormComponent {
+
+  text: string;
+
+  constructor() {
+  }
+
+  register() {
+    
+  }
+
+}
